6b23d2d0b1ff0cf8f04100e53fd4ac7c9d81af51
Jara <joajara2025@inf.udec.cl>
2026-09-17 18:56:41 -0300

    feat: Implementación del separador de inputs

--- main.c
+++ main.c
@@ -12,23 +12,45 @@ void dirprint(){
 }
 
 // Separa el String recibido del usuario en un array de Strings por cada palabra
-char** parsearCmd(char *usuario, char *retorno) {
-    char *parseado[50];
+// Devuelve la cantidad de palabras que tiene el input del usuario
+int parsearCmd(char *usuario, char retorno[100][100]) {
+	int nPalabra = 0;
+	int nCaracter = 0;
+	int dentroPalabra = 0;
+	// Se recorre cada caracter del input del usuario y se añade uno a uno a cada fila del retorno
     for(int i = 0; usuario[i] != '\0'; i++){
-        // [Por implementar]
+        if (usuario[i] == ' ' || usuario[i] == '\n') {
+			if(dentroPalabra) {
+				usuario[i] = '\0';
+				retorno[nPalabra][nCaracter] = '\0';
+				nCaracter = 0; nPalabra++;
+				dentroPalabra = 0;
+			}
+		} else {
+			retorno[nPalabra][nCaracter++] = usuario[i];
+			dentroPalabra = 1;
+		}
     }
-    return NULL;
+	retorno[nPalabra][nCaracter] = '\0';
+
+	// Se añade 1 para compensar por el index 0
+	return nPalabra + 1;
 }
 
 int main(){
-	char args[100];		// Input completo del usuario
-	char **parseado;	// Array del input por palabra
-	char *space = " ";
+	char args[100];				// Input completo del usuario
+	/*
+		[[ TODO: Se requiere que las pipes sean de límite arbitrario por lo que parseado debe estar definido como 'char *parseado[100]' ]]
+	 */
+	char parseado[100][100];	// Array del input por palabra
+
 	// Bucle principal de la shell
 	while(1) {
 		dirprint();
 		fgets(args, sizeof(args), stdin); // Lee input desde stdin y lo guarda en args
-		args[strcspn(args, "\n")] = '\0'; // 
+		args[strcspn(args, "\n")] = '\0'; // Reemplaza el primer salto de línea por ser el final de String
+		int nLineas = parsearCmd(args, parseado);
+
 		if(strcmp(args, "exit") == 0) {
 			return 0;
 		}
